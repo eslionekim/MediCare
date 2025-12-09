90fe6f3d7750241b9d26153bfd260302ba276460
admin <133757594+eslionekim@users.noreply.github.com>
2025-12-09 17:31:53 +0900

    의사-> 휴가 신청

--- erp/src/main/java/com/example/erp/Vacation/VacationController.java
+++ erp/src/main/java/com/example/erp/Vacation/VacationController.java
@@ -1,8 +1,12 @@
 package com.example.erp.Vacation;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +15,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.erp.Chart.ChartService;
+import com.example.erp.Status_code.Status_code;
 import com.example.erp.Status_code.Status_codeRepository;
+import com.example.erp.User_account.User_account;
+import com.example.erp.Vacation_type.Vacation_type;
+import com.example.erp.Vacation_type.Vacation_typeRepository;
 
 import lombok.RequiredArgsConstructor;
+
 
 @Controller
 @RequiredArgsConstructor
 public class VacationController {
 	private final VacationRepository vacationRepository;
+	private final Vacation_typeRepository vacation_typeRepository;
 	private final Status_codeRepository status_codeRepository;
 	
 	@GetMapping("/hr/vacationList") //인사 -> 휴가 리스트
@@ -29,7 +39,7 @@ public class VacationController {
     }
 	
 	// 상태 변경 AJAX
-    @PostMapping("/hr/vacation/updateStatus")
+    @PostMapping("/hr/vacation/updateStatus") //인사 -> 휴가 승인/취소/반려
     @ResponseBody
     public Map<String, Object> updateStatus(@RequestBody Map<String, String> params) {
         try {
@@ -38,7 +48,9 @@ public class VacationController {
 
             Vacation vacation = vacationRepository.findById(vacationId).orElse(null);
             if (vacation != null) {
-                vacation.setStatus_code(status_codeRepository.findByCode(status)); //상태코드 설정
+            	Status_code statusCode = status_codeRepository.findByCode(status) // 상태코드 설정
+            	        .orElseThrow(() -> new RuntimeException("상태코드 없음"));
+            	vacation.setStatus_code(statusCode);
                 vacationRepository.save(vacation); //설정한대로 저장
                 return Map.of("success", true); //성공 여부를 JSON으로 반환
             }
@@ -46,5 +58,53 @@ public class VacationController {
             e.printStackTrace();
         }
         return Map.of("success", false);
+    }
+    
+    // 의사 -> 휴가 신청
+    @GetMapping("/doctor/applyVacation")
+    public String applyVacation(Model model) {
+        String user_id = SecurityContextHolder.getContext().getAuthentication().getName();
+        List<Vacation> vacation = vacationRepository.findVacationByUserId(user_id);
+        List<Vacation_type> vacationTypes = vacation_typeRepository.findAll();
+        
+        model.addAttribute("vacation", vacation);
+        model.addAttribute("vacationTypes", vacationTypes);
+        return "doctor/applyVacation";
+    }
+    
+    // 의사 -> 휴가 신청 -> 폼 제출
+    @PostMapping("/doctor/applyVacation")
+    @ResponseBody // 반환값을 JSON형태(키-값)로 전달
+    public Map<String,Object> applyVacation(@RequestBody Map<String,String> body) {
+        Map<String,Object> result = new HashMap<>(); // 키-값 
+        try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            String user_id = auth.getName(); //로그인 한 사용자의 user_id
+
+            User_account user = new User_account(); 
+            user.setUser_id(user_id); //새로 생성한 객체에 user_id 설정
+
+            Vacation_type vt = vacation_typeRepository.findByTypeName(body.get("type_name")) //type_name으로 vacation_type 찾기
+                    .orElseThrow(() -> new RuntimeException("존재하지 않는 휴가분류"));
+
+            Status_code status_code = status_codeRepository.findByCode("VAC_APPROVED_REQUESTED") //코드로 status_code찾기
+                    .orElseThrow(() -> new RuntimeException("상태코드 없음"));
+
+            Vacation v = new Vacation();
+            v.setUser_account(user); 
+            v.setVacation_type(vt);
+            v.setStart_date(LocalDate.parse(body.get("start_date")));
+            v.setEnd_date(LocalDate.parse(body.get("end_date")));
+            v.setStatus_code(status_code);
+            v.setReason(body.get("reason"));
+
+            vacationRepository.save(v);
+
+            result.put("success", true); // 저장 성공 시 
+        } catch(Exception e){
+            result.put("success", false);
+            result.put("message", e.getMessage());
+        }
+        return result;
     }
 }
--- erp/src/main/java/com/example/erp/Vacation/VacationRepository.java
+++ erp/src/main/java/com/example/erp/Vacation/VacationRepository.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VacationRepository extends JpaRepository<Vacation, Long> {
 	@Query("SELECT v FROM Vacation v JOIN FETCH v.user_account u JOIN FETCH v.vacation_type vt JOIN FETCH v.status_code sc LEFT JOIN FETCH u.staff_profile sp LEFT JOIN FETCH sp.department d")
 	List<Vacation> vacationList();
+	
+	// 의사 -> 휴가 신청 -> 로그인한 직원(user_id)의 휴가만 조회
+    @Query("SELECT v FROM Vacation v JOIN FETCH v.vacation_type vt JOIN FETCH v.status_code sc WHERE v.user_account.user_id = :user_id")
+    List<Vacation> findVacationByUserId(@Param("user_id") String user_id);
 }
