d4bb37a9bba872c326a5d0e4fc3d413c3d492aa9
20dlwnsrhkd <jimmy010011@gmail.com>
2025-12-09 15:22:10 +0900

    접수

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-## 25.12.01 ~ 25.12.08 김은서 : [의사] 금일 진료 리스트, 차트 작성 및 수정, 차트 조회, 전체 진료 리스트 
+## 25.12.01 ~ 25.12.08 김은서 : [의사] 금일 진료 리스트, 차트 작성 및 수정, 차트 조회, 전체 진료 리스트
+
 금일 진료 리스트 http://localhost:8080/doctor/todayVisits
 (진료 시작 버튼 클릭 시) 차트 작성 및 수정 http://localhost:8080/doctor/chartWrite?visit_id=방문id&patient_id=환자id
 (차트 저장 및 전체 진료 리스트에서 클릭 시) 차트 조회 http://localhost:8080/doctor/chartView?visit_id=방문id&patient_id==환자id
--- erp/src/main/java/com/example/erp/Visit/VisitService.java
+++ erp/src/main/java/com/example/erp/Visit/VisitService.java
@@ -14,91 +14,91 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class VisitService { 
-	private final VisitRepository visitRepository;
-	
-	@Transactional(readOnly=true) //트랜젝션(조회용)
-	public List<TodayVisitDTO> getTodayVisitList(){ //의사->금일 진료 리스트
-		LocalDate today = LocalDate.now(); // 오늘 날짜 -> 최종내원일에 쓰임
-		LocalDateTime todayStart = today.atStartOfDay(); // 오늘 날짜를 LocalDateTime으로 바꿈, 진료시간으로 설정된거랑 비교하려면 쩔수없음
+public class VisitService {
+    private final VisitRepository visitRepository;
 
-		LocalDateTime start = today.atStartOfDay();
-		LocalDateTime end = today.plusDays(1).atStartOfDay().minusSeconds(1);
-		List<Visit> todayVisits = visitRepository.findByVisitDate(start, end); // 금일 진료 리스트 조회
+    @Transactional(readOnly = true) // 트랜젝션(조회용)
+    public List<TodayVisitDTO> getTodayVisitList() { // 의사->금일 진료 리스트
+        LocalDate today = LocalDate.now(); // 오늘 날짜 -> 최종내원일에 쓰임
+        LocalDateTime todayStart = today.atStartOfDay(); // 오늘 날짜를 LocalDateTime으로 바꿈, 진료시간으로 설정된거랑 비교하려면 쩔수없음
+
+        LocalDateTime start = today.atStartOfDay();
+        LocalDateTime end = today.plusDays(1).atStartOfDay().minusSeconds(1);
+
+        List<Visit> todayVisits = visitRepository.findByVisitDate(start, end); // 금일 진료 리스트 조회
         List<TodayVisitDTO> result = new ArrayList<>(); // 추출한거 담을 결과물
-        LocalDateTime first = null; //가장 빠른 시간
-        
+        LocalDateTime first = null; // 가장 빠른 시간
+
         // 진료대기 리스트에서 가장 빠른 시간 찾기
         for (Visit v : todayVisits) {
             if ("대기 중".equals(v.getStatus_code().getName())) {
                 if (first == null || v.getVisit_datetime().isBefore(first)) {
-                	first = v.getVisit_datetime();
+                    first = v.getVisit_datetime();
                 }
             }
         }
-        
+
         for (Visit v : todayVisits) { // 금일 진료 리스트에 담긴거 하나씩 뽑기
             TodayVisitDTO dto = new TodayVisitDTO(); // 금일 진료 컬럼 dto
             dto.setVisit_id(v.getVisit_id());
             dto.setPatient_id(v.getPatient().getPatient_id()); // 환자번호
-            dto.setName(v.getPatient().getName());             // 환자명
-            dto.setGender(v.getPatient().getGender());         // 성별
+            dto.setName(v.getPatient().getName()); // 환자명
+            dto.setGender(v.getPatient().getGender()); // 성별
             dto.setBirth_date(v.getPatient().getBirth_date());// 생년월일
             dto.setVisit_time(v.getVisit_datetime().toLocalTime());// 접수시간
-            dto.setVisit_type(v.getVisit_type());                // 진료 구분
-            dto.setNote(v.getNote());                             // 내원 사유
+            dto.setVisit_type(v.getVisit_type()); // 진료 구분
+            dto.setNote(v.getNote()); // 내원 사유
             dto.setStatus_name(v.getStatus_code().getName()); // 상태 코드
-            
-            // 최종 내원일 조회 (오늘 방문 제외)
-            LocalDateTime lastVisit = visitRepository.findLastVisitBeforeToday(v.getPatient().getPatient_id(), todayStart);
-            dto.setLast_visit(lastVisit != null ? lastVisit.toLocalDate() : null); // 초진이면 null
 
-            dto.setFirst( "대기 중".equals(v.getStatus_code().getName()) && v.getVisit_datetime().equals(first));
-            			// "대기 중"만 선별 && 가장 빠른 visit_datetime인거 저장
-            
+            // 최종 내원일 조회 (오늘 방문 제외)
+            LocalDateTime lastVisit = visitRepository.findLastVisitBeforeToday(v.getPatient().getPatient_id(), today);
+            dto.setLast_visit(lastVisit != null ? lastVisit.toLocalDate() : null);
+
+            dto.setFirst("대기 중".equals(v.getStatus_code().getName()) && v.getVisit_datetime().equals(first));
+            // "대기 중"만 선별 && 가장 빠른 visit_datetime인거 저장
+
             // 차트ID가 있으면 넣기, 없으면 null
             if (!v.getChart().isEmpty()) {
                 dto.setChart_id(v.getChart().get(0).getChart_id());
             } else {
                 dto.setChart_id(null);
             }
-            
+
             result.add(dto); // DTO 리스트에 추가
         }
-        
-        Map<String, Integer> statusPriority = Map.of( //상태 우선순위
+
+        Map<String, Integer> statusPriority = Map.of( // 상태 우선순위
                 "대기 중", 1,
                 "진료 중", 2,
                 "진료 종료", 3,
-                "수납/청구 완료", 4
-        );
-        
-        result.sort( //sort : 정렬
-        	    Comparator //비교자
-        	        .comparingInt((TodayVisitDTO dto) -> statusPriority.getOrDefault(dto.getStatus_name(), 99)) // 번호 기준
-        	        //접근 정수비교                       람다  우선순위      접근  조회            상태             없음 기본
-        	        .thenComparing(TodayVisitDTO::getVisit_time) // LocalTime 기준 정렬
-        	        // 후 비교        타입            방문시간
-        );
+                "수납/청구 완료", 4);
 
+        result.sort( // sort : 정렬
+                Comparator // 비교자
+                        .comparingInt((TodayVisitDTO dto) -> statusPriority.getOrDefault(dto.getStatus_name(), 99)) // 번호
+                                                                                                                    // 기준
+                        // 접근 정수비교 람다 우선순위 접근 조회 상태 없음 기본
+                        .thenComparing(TodayVisitDTO::getVisit_time) // LocalTime 기준 정렬
+        // 후 비교 타입 방문시간
+        );
 
         return result; // 최종 DTO 리스트 반환
-	}
+    }
 
-    public List<Visit> findByPatientId(Long patient_id) { //의사->금일 진료 리스트->진료 시작 -> 환자 과거 방문 기록 조회
+    public List<Visit> findByPatientId(Long patient_id) { // 의사->금일 진료 리스트->진료 시작 -> 환자 과거 방문 기록 조회
         return visitRepository.findByPatientId(patient_id);
     }
-    
+
     public Visit findById(Long visit_id) { // 의사 -> 차트 작성 -> 방문 정보 (진료 유형, 내원 경로) 불러오기
-    	return visitRepository.findById(visit_id)
-                .orElseThrow(() -> new RuntimeException("Visit not found: " + visit_id)); //없으면
+        return visitRepository.findById(visit_id)
+                .orElseThrow(() -> new RuntimeException("Visit not found: " + visit_id)); // 없으면
     }
-    
+
     public Visit findWithDepartmentAndInsurance(Long visit_id) { // 의사 -> 차트 작성 -> 진료과,보험명 불러오기
         return visitRepository.findWithDepartmentAndInsurance(visit_id);
     }
-    
-    @Transactional(readOnly = true) //의사 -> 전체 진료 리스트
+
+    @Transactional(readOnly = true) // 의사 -> 전체 진료 리스트
     public List<AllVisitDTO> getAllVisitList() {
         List<Visit> visits = visitRepository.findAllVisits(); // 레포지토리에서 연관관계 한번에 조회
         List<AllVisitDTO> result = new ArrayList<>();
